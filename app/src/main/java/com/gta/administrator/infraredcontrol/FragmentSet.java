5ffb01acf370818b229c4ec4c0da38263a1e4e76
314625433@qq.com <314625433@qq.com>
2016-10-04 13:18:58 +0800

    添加了第一界面选项卡滑动

---
+++ app/src/main/java/com/gta/administrator/infraredcontrol/FragmentSet.java
@@ -0,0 +1,22 @@
+package com.gta.administrator.infraredcontrol;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yanwen on 16/10/4.
+ */
+public class FragmentSet extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_set, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/gta/administrator/infraredcontrol/Main1Activity.java
@@ -0,0 +1,140 @@
+package com.gta.administrator.infraredcontrol;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main1Activity extends AppCompatActivity implements View.OnClickListener{
+
+    private Toolbar toolbar;
+    private ViewPager my_view_pager;
+
+    private List<Fragment> fragments;
+
+    private MyGradLayoutItem home_button, device_button, set_button;
+    private List<View> buttons = new ArrayList<>(3);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.menu_main1);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main1Activity.this, MainActivity.class));
+            }
+        });
+
+
+        my_view_pager = (ViewPager) findViewById(R.id.my_viewpage);
+
+        fragments = new ArrayList<>();
+        fragments.add(new FragmentHome());
+        fragments.add(new FragmentDevices());
+        fragments.add(new FragmentSet());
+
+        my_view_pager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        my_view_pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < buttons.size(); ++i) {
+                    if (i == position) {
+                        buttons.get(position).setSelected(true);
+                    } else {
+                        buttons.get(i).setSelected(false);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        my_view_pager.setCurrentItem(0);
+        home_button = (MyGradLayoutItem) findViewById(R.id.home_button);
+        home_button.setOnClickListener(this);
+        device_button = (MyGradLayoutItem) findViewById(R.id.device_button);
+        device_button.setOnClickListener(this);
+        set_button = (MyGradLayoutItem) findViewById(R.id.set_button);
+        set_button.setOnClickListener(this);
+        buttons.add(home_button);
+        buttons.add(device_button);
+        buttons.add(set_button);
+        buttons.get(0).setSelected(true);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.home_button:
+                my_view_pager.setCurrentItem(0);
+                break;
+            case R.id.device_button:
+                my_view_pager.setCurrentItem(1);
+                break;
+            case R.id.set_button:
+                my_view_pager.setCurrentItem(2);
+                break;
+        }
+
+    }
+
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+
+
+
+
+}
